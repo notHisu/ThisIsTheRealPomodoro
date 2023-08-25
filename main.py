066f5d12ac8be0d0c497151c80f94149ade390a9
notHisu <nothisu.dev@gmail.com>
2023-08-26 03:37:09 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,104 @@
+# This Python file uses the following encoding: utf-8
+
+#VM Activate
+#source venv/bin/activate
+import sys
+
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QTimer
+
+
+# Important:
+# You need to run the following command to generate the ui_form.py file
+#     pyside6-uic form.ui -o ui_form.py, or
+#     pyside2-uic form.ui -o ui_form.py
+from ui_form import Ui_Main
+
+class Main(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_Main()
+        self.ui.setupUi(self)
+
+        #Create timer
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_timer)
+        self.counter = 0
+
+        self.session_length = 25 * 60
+        self.break_length = 5 * 60
+        self.timer_running = False
+
+        #Setup timer label
+        minutes = self.session_length // 60
+        seconds = self.session_length % 60
+        self.ui.labelTime.setText(f"{minutes:02d}:{seconds:02d}")
+
+        self.ui.buttonStart.clicked.connect(self.toggle_timer)
+        self.ui.buttonReset.clicked.connect(self.reset_timer)
+        self.ui.buttonPreset15.clicked.connect(lambda: self.set_preset_time(15))
+        self.ui.buttonPreset30.clicked.connect(lambda: self.set_preset_time(30))
+        self.ui.buttonPreset45.clicked.connect(lambda: self.set_preset_time(45))
+
+        self.update_preset_buttons()
+
+    def start_timer(self):
+        if not self.timer_running:
+            self.timer.start(1000)
+            self.timer_running = True
+            self.ui.buttonStart.setText("Pause")
+
+            #Setup progress bar
+            self.ui.progressBar.setMaximum(self.session_length)
+            self.ui.progressBar.setValue(self.session_length)
+            self.update_preset_buttons()
+
+    def pause_timer(self):
+        if self.timer_running:
+            self.timer.stop()
+            self.timer_running = False
+            self.ui.buttonStart.setText("Start")
+
+    def toggle_timer(self):
+        if self.timer_running:
+            self.pause_timer()
+        else:
+            self.start_timer()
+
+    def reset_timer(self):
+        self.session_length = 25 * 60
+        self.ui.labelTime.setText("00:00")
+        self.timer.stop()
+
+    def update_timer(self):
+        self.session_length -= 1
+        minutes = self.session_length // 60
+        seconds = self.session_length % 60
+        self.ui.labelTime.setText(f"{minutes:02d}:{seconds:02d}")
+        self.ui.progressBar.setValue(self.session_length)
+        if self.session_length == 0:
+            self.timer.stop()
+            self.counter += 1
+            self.ui.labelCounter.setText(f"Counter: {self.counter}")
+            self.timer_running = False
+
+    def set_preset_time(self, presetTime):
+        self.session_length = presetTime * 60
+        minutes = self.session_length // 60
+        seconds = self.session_length % 60
+        self.ui.labelTime.setText(f"{minutes:02d}:{seconds:02d}")
+        self.ui.progressBar.setMaximum(self.session_length)
+        self.ui.progressBar.setValue(self.session_length)
+
+    def update_preset_buttons(self):
+        self.ui.buttonPreset15.setEnabled(not self.timer_running)
+        self.ui.buttonPreset30.setEnabled(not self.timer_running)
+        self.ui.buttonPreset45.setEnabled(not self.timer_running)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = Main()
+    widget.show()
+    sys.exit(app.exec())
---
+++ ui_form.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.5.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QGroupBox, QLabel, QMainWindow,
+    QMenu, QMenuBar, QProgressBar, QPushButton,
+    QSizePolicy, QStatusBar, QVBoxLayout, QWidget)
+
+class Ui_Main(object):
+    def setupUi(self, Main):
+        if not Main.objectName():
+            Main.setObjectName(u"Main")
+        Main.resize(478, 436)
+        self.actionQuit = QAction(Main)
+        self.actionQuit.setObjectName(u"actionQuit")
+        self.actionAbout = QAction(Main)
+        self.actionAbout.setObjectName(u"actionAbout")
+        self.centralwidget = QWidget(Main)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.progressBar = QProgressBar(self.centralwidget)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setGeometry(QRect(100, 220, 251, 20))
+        self.progressBar.setValue(24)
+        self.groupBoxPresets = QGroupBox(self.centralwidget)
+        self.groupBoxPresets.setObjectName(u"groupBoxPresets")
+        self.groupBoxPresets.setGeometry(QRect(230, 70, 127, 137))
+        self.verticalLayout_3 = QVBoxLayout(self.groupBoxPresets)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.buttonPreset15 = QPushButton(self.groupBoxPresets)
+        self.buttonPreset15.setObjectName(u"buttonPreset15")
+
+        self.verticalLayout_3.addWidget(self.buttonPreset15)
+
+        self.buttonPreset30 = QPushButton(self.groupBoxPresets)
+        self.buttonPreset30.setObjectName(u"buttonPreset30")
+
+        self.verticalLayout_3.addWidget(self.buttonPreset30)
+
+        self.buttonPreset45 = QPushButton(self.groupBoxPresets)
+        self.buttonPreset45.setObjectName(u"buttonPreset45")
+
+        self.verticalLayout_3.addWidget(self.buttonPreset45)
+
+        self.widget = QWidget(self.centralwidget)
+        self.widget.setObjectName(u"widget")
+        self.widget.setGeometry(QRect(100, 80, 101, 131))
+        self.verticalLayout_2 = QVBoxLayout(self.widget)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.labelCounter = QLabel(self.widget)
+        self.labelCounter.setObjectName(u"labelCounter")
+
+        self.verticalLayout_2.addWidget(self.labelCounter)
+
+        self.labelTime = QLabel(self.widget)
+        self.labelTime.setObjectName(u"labelTime")
+
+        self.verticalLayout_2.addWidget(self.labelTime)
+
+        self.buttonStart = QPushButton(self.widget)
+        self.buttonStart.setObjectName(u"buttonStart")
+
+        self.verticalLayout_2.addWidget(self.buttonStart)
+
+        self.buttonReset = QPushButton(self.widget)
+        self.buttonReset.setObjectName(u"buttonReset")
+
+        self.verticalLayout_2.addWidget(self.buttonReset)
+
+        Main.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(Main)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 478, 37))
+        self.menuFile = QMenu(self.menubar)
+        self.menuFile.setObjectName(u"menuFile")
+        Main.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(Main)
+        self.statusbar.setObjectName(u"statusbar")
+        Main.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menuFile.addAction(self.actionAbout)
+        self.menuFile.addAction(self.actionQuit)
+
+        self.retranslateUi(Main)
+
+        QMetaObject.connectSlotsByName(Main)
+    # setupUi
+
+    def retranslateUi(self, Main):
+        Main.setWindowTitle(QCoreApplication.translate("Main", u"Main", None))
+        self.actionQuit.setText(QCoreApplication.translate("Main", u"Quit", None))
+        self.actionAbout.setText(QCoreApplication.translate("Main", u"About", None))
+        self.groupBoxPresets.setTitle(QCoreApplication.translate("Main", u"Presets", None))
+        self.buttonPreset15.setText(QCoreApplication.translate("Main", u"15 minutes", None))
+        self.buttonPreset30.setText(QCoreApplication.translate("Main", u"30 minutes", None))
+        self.buttonPreset45.setText(QCoreApplication.translate("Main", u"45 minutes", None))
+        self.labelCounter.setText(QCoreApplication.translate("Main", u"Counter: 0", None))
+        self.labelTime.setText(QCoreApplication.translate("Main", u"00:00", None))
+        self.buttonStart.setText(QCoreApplication.translate("Main", u"Start", None))
+        self.buttonReset.setText(QCoreApplication.translate("Main", u"Reset", None))
+        self.menuFile.setTitle(QCoreApplication.translate("Main", u"File", None))
+    # retranslateUi
+
